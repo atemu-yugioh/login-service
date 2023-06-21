6f2cdd78783e195f97b777a201c64bfeb16224b6
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-06-21 11:10:32 +0700

    #6 login feature

--- src/services/auth.sevice.js
+++ src/services/auth.sevice.js
@@ -3,10 +3,12 @@ const authRepositories = require('../models/repositories/user.repositories')
 const keyTokenRepositories = require('../models/repositories/keyToken.repositories')
 const { newMongoObjectId, getInfoData } = require('../utils')
 const { hashPassword, createHEXKey, createTokenPair } = require('../utils/auth')
+const { isMatchPassword } = require('../utils/auth')
 
 class AuthService {
   constructor() {}
 
+  // Sign up
   static SignUp = async ({ email, name, password, phone, roles = ['user'], ...other }) => {
     // check user exist
     const userFound = await authRepositories.findByEmail(email)
@@ -69,6 +71,54 @@ class AuthService {
           accessToken,
           refreshToken
         }
+      }
+    }
+  }
+
+  // Login
+  static login = async ({ email, password }) => {
+    // check user exist
+    const userFound = await authRepositories.findByEmail(email)
+
+    if (!userFound) {
+      throw new BadRequestError('Error: User not registered')
+    }
+
+    const isMath = isMatchPassword(password, userFound.password)
+
+    if (!isMath) {
+      throw new BadRequestError('Error: Password invalid')
+    }
+
+    // generate publicKey and privateKey
+    const { publicKey, privateKey } = createHEXKey()
+
+    // generate token pair
+    const { _id: userId } = userFound
+    const { accessToken, refreshToken } = await createTokenPair({ userId: userId, email }, publicKey, privateKey)
+
+    // create or update KeyToken
+    const keyToken = await keyTokenRepositories.createKeyToken({
+      userId,
+      publicKey,
+      privateKey,
+      refreshToken,
+      createdBy: userId,
+      modifiedBy: userId
+    })
+
+    if (!keyToken) {
+      throw new BadRequestError('Error: keyToken Error')
+    }
+
+    return {
+      user: getInfoData({
+        object: userFound,
+        fields: ['email', 'name', 'phone', 'address', 'avatar']
+      }),
+      tokens: {
+        accessToken,
+        refreshToken
       }
     }
   }
